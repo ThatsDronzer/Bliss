8332d9c0d6d97a17750de04eb124917aed11a4dc
Ayrush verma <av577294@gmail.com>
2025-07-06 11:43:28 +0530

    fixed db issue hopefully

--- lib/config/db.ts
+++ lib/config/db.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI,{
+      dbName:'bliss app',
+    })
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
