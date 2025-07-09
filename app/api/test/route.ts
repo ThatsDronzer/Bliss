a6c8a79af79067247e12e2fd44e8cc4aa8dc5fe0
Aayush-Aman <amanaayush171@gmail.com>
2025-07-09 10:58:27 +0530

    removed the vendor verification schema and changes in the vendor schema

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -5,8 +5,8 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     await connectDB();
-    return NextResponse.json({ message: "✅ Connected to MongoDB" });
+    return NextResponse.json({ message: " Connected to MongoDB" });
   } catch (error) {
-    return NextResponse.json({ error: "❌ Failed to connect" }, { status: 500 });
+    return NextResponse.json({ error: " Failed to connect" }, { status: 500 });
   }
 }
