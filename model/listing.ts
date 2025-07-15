b4379f112571ded6f40365fedc74daa4cc462e75
harpreet khatter <khatterharpreet@gmail.com>
2025-07-15 14:38:50 +0530

    created get and post route for listings

--- model/listing.ts
+++ model/listing.ts
@@ -4,7 +4,7 @@ export interface IListing extends Document {
   title: string;
   description: string;
   price: number;
-  images: {
+  images?: {
     url: string;
     // filename: string;
   }[];
@@ -37,16 +37,16 @@ const listingSchema = new Schema<IListing>(
     },
 
     images: [
-      // {
-      //   url: { type: String, required: true },
-      //   filename: { type: String, required: true },
-      // },
       {
-        url:"https://images.unsplash.com/photo-1492684223066-81342ee5ff30?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlbnR8ZW58MHx8MHx8fDA%3D"
+        url: { type: String, required: true },
+        // filename: { type: String, required: true },
       },
-      {
-        url:"https://images.pexels.com/photos/2747449/pexels-photo-2747449.jpeg?cs=srgb&dl=pexels-wolfgang-1002140-2747449.jpg&fm=jpg"
-      }
+      // {
+      //   url:"https://images.unsplash.com/photo-1492684223066-81342ee5ff30?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlbnR8ZW58MHx8MHx8fDA%3D"
+      // },
+      // {
+      //   url:"https://images.pexels.com/photos/2747449/pexels-photo-2747449.jpeg?cs=srgb&dl=pexels-wolfgang-1002140-2747449.jpg&fm=jpg"
+      // }
     ],
 
     isActive: {
@@ -80,5 +80,5 @@ const listingSchema = new Schema<IListing>(
   { timestamps: true }
 );
 
-const Listing = model<IListing>('Service', listingSchema);
+const Listing = mongoose.models.Service || model<IListing>('Service', listingSchema);
 export default Listing;
